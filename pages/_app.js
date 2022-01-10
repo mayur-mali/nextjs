3241797a75c62591450c386d4abbb52a2d33acd2
vampire-mayu <mayurmali31@gmail.com>
2022-01-10 22:23:16 +0530

    testing with new files

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,35 @@
-import '../styles/globals.css'
+import { useState, useEffect } from "react";
+import defautLayout from "../layouts/defautLayout";
+import loginLayout from "../layouts/loginLayout";
+import withoutNavLayout from "../layouts/withoutNavLayout";
+import "../styles/globals.css";
+import Spinner from "../components/Spinner";
+
+const layouts = {
+  L1: defautLayout,
+  L2: loginLayout,
+  L3: withoutNavLayout,
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+  const Layout = layouts[Component.layout] || ((children) => <>{children}</>);
+  return (
+    <>
+      {loading && <Spinner />}
+      {!loading && (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      )}
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./layouts/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundColor: {
+        "inter-blue": "#09C0FA",
+        "inter-yellow": "#FFB647",
+        "inter-black": "#222831",
+        "inter-white": "#FFFFFF",
+      },
+      fontFamily: {
+        workSans: ['"Work Sans"', "sans-serif"],
+        EbG: ['"EB Garamond"', "serif"],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/line-clamp")],
+};
